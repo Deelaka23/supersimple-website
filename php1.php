09e408b21df9eb2cde2891f167ccb80faf59b564
Deelaka23 <deelakanelumdeniye@gmail.com>
2024-06-11 09:05:47 +0530

    Update php1.php

--- php1.php
+++ php1.php
@@ -2,10 +2,22 @@
     $uname = $_POST['name'];
     $password = $_POST['pw'];
 
-    echo $uname;
-    echo $password;
+    $conn = mysqli_connect('localhost','root','','database');
 
+    if (!$conn){
+            die ("connection failed:".mysqli_connect_error());
+    }
+    else{
+        echo "working!!!";
+    }
 
+    $sql="INSERT INTO user VALUES ($uname,$password);";
+    
+    $result = $conn->query($sql);
+    if ($result) {
+        echo "data entered!!!";
+    }
+    
 
 
 
